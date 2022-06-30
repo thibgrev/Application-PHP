1f3225078aade03826f542fd6e34f9179c76e722
thibgrev <thibgrev@hotmail.com>
2022-06-30 18:20:05 +0200

    Mise de ENV a la place de SERVER

--- Variable_d_environnement/index.php
+++ Variable_d_environnement/index.php
@@ -1,15 +1,10 @@
 <?php
-    // Example use of getenv()
-    $ip = getenv('REMOTE_ADDR');
-
-    echo 'L ip est '.$ip;
-
     // Or simply use a Superglobal ($_SERVER or $_ENV)
     $ip = $_SERVER['REMOTE_ADDR'];
 
     echo 'L ip est '.$ip;
 
-    $tg_variable = $_SERVER['TGVARIABLE'];
+    $tg_variable = $_ENV['TGVARIABLE'];
 
     echo 'La variable d environnement tg_variable est "'.$tg_variable.'".';
 ?>
