4c4afe915f1e1ce302311d1a2a00072475d99aae
thibgrev <thibgrev@hotmail.com>
2022-06-30 17:41:23 +0200

    Oranisation en repertoire

---
+++ Variable_d_environnement/index.php
@@ -0,0 +1,23 @@
+<?php
+  $hstnme = gethostname();
+?>
+  <html>
+    <title>
+      Application PHP
+    </title>
+    <head>
+      <style>
+        h1 {text-align: center;}
+      </style>
+      <h1>-Application pour comprendre le changement de container-</h1>
+    </head>
+    <body>
+  </html>
+  <?php
+      echo '<hr>';
+      echo 'Le contener qui prend en charge cette requete web est "<b>'.$hstnme.'</b>".';
+      echo '<hr>';
+  ?>
+  <html>
+    </body>
+  </html>
