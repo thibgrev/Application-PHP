5012759202a84c5b314876e9b8c09a485d68a516
thibgrev <85991797+thibgrev@users.noreply.github.com>
2022-06-30 17:13:17 +0200

    Ajout de champ en HTML

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-  echo "test";
   $hstnme = gethostname();
-  echo 'Le contener qui prend en charge cette requete web est'.$hstnme.'!';
 ?>
+  <html>
+    <title>
+      Application PHP
+    </title>
+    <head>
+      -Application pour comprendre le changement de container-{text-align: center;}
+    </head>
+    <body>
+  </html>
+  <?php
+      echo '<hr>';
+      echo 'Le contener qui prend en charge cette requete web est "<b>'.$hstnme.'</b>"!';
+      echo '<hr>';
+  ?>
+  <html>
+    </body>
+  </html>
